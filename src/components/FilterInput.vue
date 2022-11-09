d893ec0f1411f45a73c5d84ca5acd39aa1fdb566
LeanneTN <2321604632@qq.com>
2022-11-09 23:12:20 +0800

    side bar components

---
+++ src/components/FilterInput.vue
@@ -0,0 +1,17 @@
+<template>
+  <el-button color="#626aef" :dark="isDark" plain>Include</el-button>
+  <el-button color="#626aef" :dark="isDark" plain>Exclude</el-button>
+</template>
+
+<script>
+export default {
+  name: "FilterInput"
+}
+</script>
+<script setup>
+  import { isDark } from '~/composables/dark'
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/SideBar.vue
@@ -0,0 +1,21 @@
+<template>
+  <FilterInput></FilterInput>
+  <MyList></MyList>
+</template>
+
+<script>
+import FilterInput from "@/components/FilterInput";
+import MyList from "@/components/MyList";
+
+export default {
+  name: "SideBar",
+  components: {
+    FilterInput,
+    MyList
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
