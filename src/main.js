45db01e0c864839fe23ceb87289fd6fd242f6790
LeanneTN <2321604632@qq.com>
2022-11-09 22:05:22 +0800

    element ui

--- src/main.js
+++ src/main.js
@@ -1,4 +1,14 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import Vue from 'vue'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css'
+
+Vue.use(ElementUI);
+
+new Vue({
+    el: '#app',
+    render: h => h(App)
+})
 
 createApp(App).mount('#app')
