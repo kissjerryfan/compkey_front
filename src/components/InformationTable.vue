df24ab771cee67c8884b4d7c14d7fdd5a2f453ab
Dreamer5201314 <1085011285@qq.com>
2022-11-11 19:59:20 +0800

    push table method

--- src/components/InformationTable.vue
+++ src/components/InformationTable.vue
@@ -2,7 +2,7 @@
   <div id="top-color">
 
   </div>
-  <el-table :data="tableData" height="612" style="width: 100%">
+  <el-table :data="tableData" @row-click="handleTableRow" height="612" style="width: 100%">
     <el-table-column prop="seedWords" label="SeedWords" width="700" />
     <el-table-column prop="compWords" label="CompWords" width="180" />
     <el-table-column prop="comp" label="Comp" />
@@ -90,9 +90,24 @@ const tableData = [
 </script>
 <script>
 export default {
-  name: "InformationTable"
+  name: "InformationTable",
+  data() {
+    return {
+      tableData: []
+    }
+  },
+  methods: {
+    handleTableRow(row, event, column) {
+      console.log(row,event,column)
+      let compList = new Object();
+      compList.seedWords = row.seedWords
+      compList.compWords = row.compWords
+      compList.comp = row.comp
+    }
+  }
 }
 </script>
+
 
 <style scoped>
   #top-color{
